6b8ea267cc28b833e7aefe4f2837b50f93c880b4
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-08-03 23:01:21 +0900

    add: サインインロジック

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -12,29 +12,78 @@ import {
   Input,
   Spacer,
   chakra,
+  useToast,
 } from '@chakra-ui/react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react'
 
 export default function Page() {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const toast = useToast()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setIsLoading(true)
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await signInWithEmailAndPassword(auth, email, password)
+      setEmail('')
+      setPassword('')
+      toast({
+        title: 'ログインしました',
+        status: 'success',
+        position: 'top',
+      })
+      // TODO: ログイン後のページに遷移する
+    } catch (error) {
+      toast({
+        title: 'エラーが発生しました',
+        status: 'error',
+        position: 'top',
+      })
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
     <Container py={14}>
       <Heading>サインイン</Heading>
-      <chakra.form>
+      <chakra.form onSubmit={handleSubmit}>
         <Spacer height={8} aria-hidden />
         <Grid gap={4}>
           <Box display={'contents'}>
             <FormControl>
               <FormLabel>メールアドレス</FormLabel>
-              <Input type={'email'} />
+              <Input
+                type={'email'}
+                name={'email'}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>パスワード</FormLabel>
-              <Input type={'password'} />
+              <Input
+                type={'password'}
+                name={'password'}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+              />
             </FormControl>
           </Box>
         </Grid>
         <Spacer height={4} aria-hidden />
         <Center>
-          <Button type={'submit'}>ログイン</Button>
+          <Button type={'submit'} isLoading={isLoading}>
+            ログイン
+          </Button>
         </Center>
       </chakra.form>
     </Container>
